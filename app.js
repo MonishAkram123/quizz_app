4b1cc1df8a555c683402612bc7806dc29b4fb3f9
monish <monishakram123@gmail.com>
2021-03-19 19:12:14 +0530

    Skeleton for Welcome page

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const appConfig = require('./config.json')
 const express = require('express')
+const hbs = require('hbs');
 
 var app = express()
+
+app.set('view engine', 'hbs')
+
+// Load the welcome page where users can login and register
+app.get("/welcome", (req, res) => {
+	res.render('welcome')
+})
 
 app.listen(appConfig.port, () => {
 	console.log(`Server is running on port: ${appConfig.port}`)
